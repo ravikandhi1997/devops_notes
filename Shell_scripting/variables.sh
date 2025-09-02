b48b465218a24cf82ea94b54701effb446c9e2bf
ravikandhi1997 <ravikandhi1997@gmail.com>
2025-09-02 16:22:15 +0530

    file added successfully

--- Shell_scripting/variables.sh
+++ Shell_scripting/variables.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-num1
-num2
-sum
-echo "enter value for num1"
+
+echo "Enter value for num1:"
 read num1
-echo "enter value for num2"
+
+echo "Enter value for num2:"
 read num2
-echo $sum=$num1+$num2
+
+# Perform addition
+sum=$((num1 + num2))
+
+echo "The sum of $num1 and $num2 is: $sum"
+
